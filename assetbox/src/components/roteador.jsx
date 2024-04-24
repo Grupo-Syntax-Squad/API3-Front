fbf3c532dda44339bb6e7eeddb4439ba1caa87fc
gabriel santos <gabriel.santos157@fatec.sp.gov.br>
2024-04-24 09:06:17 -0300

    correcao: atualizando roteador

--- assetbox/src/components/roteador.jsx
+++ assetbox/src/components/roteador.jsx
@@ -9,7 +9,7 @@ import CadastroManutenção from "./Cadastro/CadastroManutenção";
 import CadastroDestinatarios from "./Cadastro/CadastroDestinatarios";
 import Login from "./Login/Login";
 import Home from "./home";
-import Manutencao from "./Manutenção/manutenção";
+import Manutencao from "./manutenção/manutenção";
 
 export default function Roteador(){
     const [tela, setTela] = useState('Home')
