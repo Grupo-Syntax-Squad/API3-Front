e165090167192a12185a41396382a7e2791ba575
Ryan <ryanarajo@yahoo.com.br>
2024-05-02 20:31:08 -0300

    estilo[menu]: mudança do menu mobile para hamburguer

--- assetbox/src/components/Navegação/Menu.jsx
+++ assetbox/src/components/Navegação/Menu.jsx
@@ -11,7 +11,7 @@ import './menu.css';
 
 function Menu(props) {
   const [showLogout, setShowLogout] = useState(false);
-
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleSettingsClick = () => {
     setShowLogout(prevShowLogout => !prevShowLogout);
@@ -27,26 +27,33 @@ function Menu(props) {
 
   return (
     <div>
-      <nav className={`navbar  m-0 custom-background `} role="navigation" aria-label="main navigation">
-        <img src={LogoIcon} class='navbar-brand pl-6 pr-0' alt="AssetBox" onClick={(e) => props.seletorView('Home', e)} />
-        <span className="navbar-brand has-text-white navbar-item mr-2 is-size-3 p-0 has-text-weight-bold empresa is-clickable" href="" onClick={(e) => props.seletorView('Home', e)}>AssetBox</span>
-        {/* <a role="button" className={`navbar-burger `} onClick={toggleMenu} aria-label="menu" aria-expanded={isActive ? 'true' : 'false'} href='##'>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </a> */}
-        <div className='container'>
-          <div className={`navbar-menu is-flex is-justify-content-space-evenly`} id="navMenu">
-            <p className={`nav-item is-flex pr-6 pr-8 pl-6 is-size-5 has-text-weight-bold my-0 is-clickable`} href='' onClick={(e) => props.seletorView('Ativos', e)}>
+      <div className="navbar is-flex is-justify-content-space-between custom-background" role="navigation" aria-label="main navigation">
+        <div className='navbar-brand'>
+          <img src={LogoIcon} class='pl-6 pr-0' alt="AssetBox" onClick={(e) => props.seletorView('Home', e)} />
+          <span className="navbar-brand has-text-white navbar-item mr-2 is-size-3 p-0 has-text-weight-bold empresa is-clickable" 
+          href="" onClick={(e) => props.seletorView('Home', e)}>AssetBox</span>
+        </div>
+        
+        <div className={`navbar-burger burger ${showMenu ? 'is-active' : ''}`} 
+        aria-label="menu" aria-expanded={showMenu ? 'true' : 'false'} 
+        onClick={() => setShowMenu(!showMenu)}>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </div>
+
+        <div className={`navbar-menu ${showMenu ? 'is-active' : ''}`}>
+          <div className="navbar-menu is-flex is-justify-content-space-evenly" id="navMenu">
+            <p className="nav-item is-flex px-4 is-size-5 has-text-weight-bold my-0 is-clickable" href='' onClick={(e) => props.seletorView('Ativos', e)}>
               <img src={AssetsIcon} class='mr-2' alt="Ativos" />
               <p class='navbar-item has-text-white'> Ativos </p>
             </p>
-            <p className="nav-item is-flex pr-6 pl-6 is-size-5 has-text-weight-bold my-0 " href=' ' onClick={(e) => props.seletorView('Usuarios', e)}>
+            <p className="navbar-item is-flex px-4 is-size-5 has-text-weight-bold my-0 " href=' ' onClick={(e) => props.seletorView('Usuarios', e)}>
               <img src={UserIcon} class='mr-3' alt="Destinatários" />
               <p class='navbar-item has-text-white'>Usuários</p>
             </p>
-            <p className="navbar-item pr-6 pl-6 is-size-5 has-text-weight-bold my-0 mx-1" href=' ' onClick={(e) => props.seletorView('Manutenções', e)}>
+            <p className="navbar-item is-flex px-4 is-size-5 has-text-weight-bold my-0 mx-1" href=' ' onClick={(e) => props.seletorView('Manutenções', e)}>
               <img src={MainteinIcon} class='mr-3' alt="Manutenções" />
               <p class='navbar-item has-text-white'>Manutenção</p>
             </p>
@@ -72,7 +79,7 @@ function Menu(props) {
             </div>
           </div>
         </div>
-      </nav>
+      </div>
     </div>
 
 
