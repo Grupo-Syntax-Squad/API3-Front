8ede1562d1cdd20bbaa48a812ffa1286a4af398e
Gabriel Reis <gabrieldreisilva4@gmail.com>
2024-06-12 12:04:00 -0300

    func: inserindo modulo chart js

--- assetbox/src/components/Visualizar/dashboard.jsx
+++ assetbox/src/components/Visualizar/dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "./visualizar.css";
+import Chart from 'chart.js/auto';
 import axios from 'axios';
 
 export default function Dashboard({ setTela }) {
