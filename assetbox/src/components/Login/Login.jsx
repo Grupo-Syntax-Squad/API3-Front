78be74d686d0c9605a4701989cfe6807c78b2552
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-06-16 18:03:11 -0300

    correcao: Corrigir erro ao fazer login

--- assetbox/src/components/Login/Login.jsx
+++ assetbox/src/components/Login/Login.jsx
@@ -7,9 +7,10 @@ const Login = ({ setTela }) => {
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Passou aqui!")
         try {
-
             await axios.post("http://localhost:8000/autenticacao/login", {
                 email: email,
                 senha: senha
@@ -26,26 +27,26 @@ const Login = ({ setTela }) => {
     }
 
     return (
-        <form class='fundo  is-flex is-align-items-center is-justify-content-center '>
-            <div class='is-flex is-justify-content-center'>
-                <img src={logo} class='is-centered pb-6' alt="" />
+        <form className='fundo  is-flex is-align-items-center is-justify-content-center '>
+            <div className='is-flex is-justify-content-center'>
+                <img src={logo} className='is-centered pb-6' alt="" />
             </div>
-            <div class='tela-login is-flex is-justify-content-center' style={{
+            <div className='tela-login is-flex is-justify-content-center' style={{
                 backgroundColor: 'rgba(217, 217, 217, 0.2)', width: '80%', height: '60%',
             }}>
-                <div class='field-login is-justify-content-center m-auto'>
-                    <div class='is-flex justify-content-center'>
-                        <h3 class=' is-flex is-size-2 has-text-weight-bold m-auto'>Login</h3>
+                <div className='field-login is-justify-content-center m-auto'>
+                    <div className='is-flex justify-content-center'>
+                        <h3 className=' is-flex is-size-2 has-text-weight-bold m-auto'>Login</h3>
                     </div>
                     <div>
                         <p className='mt-6 ml-6 is-size-4 has-text-weight-medium entra'>E-mail</p>
-                        <input class="input-cinza mb-6 is-medium is-flex-grow-4 is-rounded" type="text" placeholder='Digite seu E-mail' style={{ width: '100%' }} onChange={(e) => setEmail(e.target.value)} required/>
+                        <input className="input-cinza mb-6 is-medium is-flex-grow-4 is-rounded" type="text" placeholder='Digite seu E-mail' style={{ width: '100%' }} onChange={(e) => setEmail(e.target.value)} required/>
                     </div>
                     <div>
                         <p className='mt-2 ml-6 is-size-4 has-text-weight-medium entra'>Senha</p>
-                        <input class="input-cinza mb-6 is-medium is-flex-grow-4 is-rounded" type="password"  placeholder='Digite sua Senha' style={{ width: '100%' }} onChange={(e) => setSenha(e.target.value)} required/>
+                        <input className="input-cinza mb-6 is-medium is-flex-grow-4 is-rounded" type="password"  placeholder='Digite sua Senha' style={{ width: '100%' }} onChange={(e) => setSenha(e.target.value)} required/>
                     </div>
-                    <button class='button is-flex m-auto' style={{ backgroundColor: '#D9D9D9', color: '#6a6a6a' }} onClick={() => handleSubmit()}>Entrar</button>
+                    <button className='button is-flex m-auto' style={{ backgroundColor: '#D9D9D9', color: '#6a6a6a' }} onClick={e => handleSubmit(e)}>Entrar</button>
                 </div>
             </div>
 
